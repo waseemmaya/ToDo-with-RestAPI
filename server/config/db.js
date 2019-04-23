d508a74db37bfb23d6c53e45c4a1923ffc855d9a
Waseem Maya <waseemmayaa@gmail.com>
2019-04-23 17:16:58 +0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb://admin:admin123@ds063546.mlab.com:63546/tododb",
+  "mongodb:mongourl with credationals",
   { useNewUrlParser: true }
 );
 
